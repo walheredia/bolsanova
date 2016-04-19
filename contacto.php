ba2f782ea9337d918b749e37352701e681ff35f7
Walter Heredia <wal_dh@hotmail.com>
2016-04-19 09:48:59 -0300

    todo

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST[‘nombre’];
+$email = $_POST[‘email’];
+$mensaje = $_POST[‘mensaje’];
+$para = ‘wal_dh@hotmail.com’;
+$titulo = ‘Nuevo Contacto - Web BolsaNova’;
+$header = ‘From: ‘ . $email;
+$msjCorreo = “Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje”;
+
+if ($_POST[‘submit’]) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo “<script language=’javascript’>
+alert(‘Mensaje enviado, muchas gracias.’);
+window.location.href = ‘http://bolsanova.com’;
+</script>”;
+} else {
+echo ‘Falló el envio’;
+}
+}
+?>

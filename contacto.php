c278dc24c9687e9929f2fe7551ea51e5ff28fddd
Walter Heredia <wal_dh@hotmail.com>
2016-09-19 15:05:48 -0300

    formulario contacto
    nuevos campos

--- contacto.php
+++ contacto.php
@@ -1,12 +1,15 @@
 <?php
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
+$provincia = $_POST['provincia'];
+$localidad = $_POST['localidad'];
 $tel = $_POST['tel'];
+$tipo = $_POST['tipo'];
 $mensaje = $_POST['mensaje'];
 $para = 'info@bolsanova.com.ar';
-$titulo = 'Nuevo Contacto - Web BolsaNova';
+$titulo = 'Nuevo Contacto - '. $tipo . ' - Web BolsaNova';
 $header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Teléfono: $tel\n $email\n Mensaje:\n $mensaje";
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Provincia: $provincia\n Localidad: $localidad\n Teléfono: $tel\n $email\n Mensaje:\n $mensaje";
 
 if ($_POST['submit']) {
 if (mail($para, $titulo, $msjCorreo, $header)) {
